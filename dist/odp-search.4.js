9cd2369aa6f5ac4264c0bab3d4641879230165f8
lennartvava <lennart_v_v@hotmail.com>
2024-09-12 06:27:37 +0000

    Autocommit builds

--- dist/odp-search.4.js
+++ dist/odp-search.4.js
@@ -7,12 +7,12 @@
 // ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"a86d7b74-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Map.vue?vue&type=template&id=a880821a&
-var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('div',{ref:"map",staticClass:"map-container"},[_c('div',{staticClass:"content-container"},[_c('div',{staticClass:"geolocate"},[_c('div',{staticClass:"tools"},[_c('button',{attrs:{"type":"button","aria-label":"Zoom in","title":"Zoom in"},on:{"click":_vm.zoomIn}},[_c('i',{staticClass:"icon-plus",attrs:{"aria-hidden":"true"}})]),_c('button',{attrs:{"type":"button","aria-label":"Zoom uit","title":"Zoom uit"},on:{"click":_vm.zoomOut}},[_c('i',{staticClass:"icon-minus",attrs:{"aria-hidden":"true"}})])])])])])])}
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"ece64d18-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/Map.vue?vue&type=template&id=7a08e85c&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('div',{ref:"map",staticClass:"map-container"},[_c('div',{staticClass:"content-container"},[_c('div',{staticClass:"geolocate"},[_c('div',{staticClass:"tools"},[_c('button',{attrs:{"type":"button","aria-label":"Zoom in","title":"Zoom in"},on:{"click":_vm.zoomIn}},[_c('i',{staticClass:"icon-plus",attrs:{"aria-hidden":"true"}})]),_c('button',{attrs:{"type":"button","aria-label":"Zoom uit","title":"Zoom uit"},on:{"click":_vm.zoomOut}},[_c('i',{staticClass:"icon-minus",attrs:{"aria-hidden":"true"}})]),_c('button',{attrs:{"type":"button","aria-label":"Ga naar huidige locatie","title":"Ga naar huidige locatie"},on:{"click":_vm.goToCurrentLocation}},[_c('i',{staticClass:"icon-marker",attrs:{"aria-hidden":"true"}})])])])])])])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/Map.vue?vue&type=template&id=a880821a&
+// CONCATENATED MODULE: ./src/components/Map.vue?vue&type=template&id=7a08e85c&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/web.dom-collections.iterator.js
 var web_dom_collections_iterator = __webpack_require__("ddb0");
@@ -260,6 +260,26 @@ const activeVectorLayer = new Vector["a" /* default */]({
       var _this$olMap$getView2;
 
       this.olMap.getView().setZoom((((_this$olMap$getView2 = this.olMap.getView()) === null || _this$olMap$getView2 === void 0 ? void 0 : _this$olMap$getView2.getZoom()) || 15) - 1);
+    },
+    goToCurrentLocation: function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          const {
+            longitude,
+            latitude
+          } = position.coords;
+          const coords = Object(proj["d" /* fromLonLat */])([longitude, latitude]);
+          this.olMap.getView().animate({
+            center: coords,
+            zoom: 14
+          });
+        }, error => {
+          console.error('Geolocation error:', error);
+          alert('Unable to retrieve your location.');
+        });
+      } else {
+        alert('Geolocation is not supported by your browser.');
+      }
     }
   },
   watch: {
